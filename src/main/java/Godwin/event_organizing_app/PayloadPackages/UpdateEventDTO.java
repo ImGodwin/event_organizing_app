5f5b694646820f403ec146f1633382e625af35e6
Godwin <godwinmail6@gmail.com>
2023-11-19 09:44:19 +0100

    completed updateevent DTO

--- src/main/java/Godwin/event_organizing_app/PayloadPackages/UpdateEventDTO.java
+++ src/main/java/Godwin/event_organizing_app/PayloadPackages/UpdateEventDTO.java
@@ -1,4 +1,11 @@
 package Godwin.event_organizing_app.PayloadPackages;
 
-public record UpdateEventDTO(@S) {
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public record UpdateEventDTO(@Size(message = "Description must be minimum 20 characters", min = 20) String description,
+                             LocalDate localDate,
+                             @Size(message = "Titile must be minimum 10 characters", min = 10) String title,
+                             String Location, int postsAvailable, String image, String password) {
 }
