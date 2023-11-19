ed6fcbf3b11688ef8eb491c6d912b05fca7d7393
Godwin <godwinmail6@gmail.com>
2023-11-19 09:34:59 +0100

    added the size annotation to some attributes in the event table

--- src/main/java/Godwin/event_organizing_app/PayloadPackages/NewEventDTO.java
+++ src/main/java/Godwin/event_organizing_app/PayloadPackages/NewEventDTO.java
@@ -1,12 +1,13 @@
 package Godwin.event_organizing_app.PayloadPackages;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
-public record NewEventDTO(@NotEmpty(message = "Please add a description") String description,
+public record NewEventDTO(@NotEmpty(message = "Please add a description") @Size(message = "Description must be minimum 20 characters", min = 20) String description,
                           @NotEmpty(message = "Please add a date")LocalDate date,
-                          @NotEmpty(message = "Please add a title") String title,
+                          @NotEmpty(message = "Please add a title") @Size(message = "Title must be min 10 characters", min = 10) String title,
                           @NotEmpty(message = "Please add a location") String Location,
                           @NotEmpty(message = "Please add number of posts avaialable") int postsAvailable){
 
